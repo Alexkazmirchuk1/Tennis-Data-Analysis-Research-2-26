c70ecf16b615c9b0ee6752b22662806aebff3a6c
Alexkazmirchuk1 <139513183+Alexkazmirchuk1@users.noreply.github.com>
2025-05-07 11:15:44 -0700

    Subplot with game or point leader, tab10

--- example_momentum_vis.py
+++ example_momentum_vis.py
@@ -4,50 +4,67 @@ from matplotlib import pyplot as plt
 from matplotlib import ticker
 import numpy as np
 
+# For the subplot image
+include_subplot = True
 
-# A few matches to try out:
-# 2021:
-# Nishioka/Bedene: 79
-# Kudla/Fokina: 3 (has 5 sets)
-# 
-# 2023:
-# Djokovic/Alcaraz: 30
-
-df_raw = tennis_data.load_2024()
+# Load match data
+df_raw = tennis_data.load_2021()
 matches = df_raw['match_id'].unique()
 
-my_match = matches[250]
+# Matches for access
+# 2021 = 124
+# 2022 = 124
+# 2023 = 126
+# 2024 = 251
+my_match = matches[124]
 
-# visualize momentum only to help validate upstream changes.
-
+# Create and fit the model
 model = dm.DynamicTennisModel(df_raw, my_match)
 model.fit()
 
-fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [3, 1]})
+# Extract player names
+p1name = model.match['player1'].iloc[0]
+p2name = model.match['player2'].iloc[0]
 
-fig, ax[0] = model.graph_momentum(ax=ax[0])
+if include_subplot:
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True, gridspec_kw={'height_ratios': [3, 1]})
 
-#######################################
-# Also try to plot unforced error diff, from the perspective of player1 (positive numbers better)
-unf_diff = np.cumsum(model.match['p2_unf_err'].values - model.match['p1_unf_err'].values)
+    # Plot performance momentum
+    fig, ax[0] = model.graph_momentum(ax=ax[0])
 
-ax[1].plot(np.arange(len(unf_diff)), unf_diff, c='white')
+    # Compute cumulative games won for each player
+    point_victors = model.match['game_victor'].values
+    p1_points = np.cumsum(point_victors == 1)
+    p2_points = np.cumsum(point_victors == 2)
 
-# fill the plot with a color showing which player currently has fewer total 
-# unforced errors to that point.
-p1_lead = unf_diff >= 0
-ax[1].fill_between(np.arange(len(unf_diff)), unf_diff, 0, where=p1_lead, color=plt.cm.Set1(0), alpha=0.5)
-ax[1].fill_between(np.arange(len(unf_diff)), 0, unf_diff, where=~p1_lead, color=plt.cm.Set1(1), alpha=0.5)
+    color_p1 = 'tab:red'
+    color_p2 = 'tab:blue'
 
-ax[1].set(yticks=[0])
+    # Plot cumulative games (or points)
+    ax[1].plot(p1_points, color=color_p1)
+    ax[1].plot(p2_points, color=color_p2)
 
-model.add_graph_decorations(ax[1])
+    # Fill between the lines
+    ax[1].fill_between(np.arange(len(p1_points)), p1_points, p2_points,
+                       where=p1_points >= p2_points, interpolate=True,
+                       color=color_p1, alpha=0.3)
+    ax[1].fill_between(np.arange(len(p1_points)), p1_points, p2_points,
+                       where=p1_points < p2_points, interpolate=True,
+                       color=color_p2, alpha=0.3)
 
-ax[0].set(ylabel="Performance", xlim=[0, model.match.shape[0]], ylim=[0, 1.1])
-ax[0].yaxis.set_major_locator(ticker.MultipleLocator(0.25))
+    # Add decorations and labels
+    ax[1].set(ylabel="Total Games Won")
+    model.add_graph_decorations(ax[1])
 
-ax[1].set(ylabel="Unforced error diff")
+    ax[0].set(ylabel="Performance", xlim=[0, model.match.shape[0]], ylim=[0, 1.1])
+    ax[0].yaxis.set_major_locator(ticker.MultipleLocator(0.25))
+    fig.align_ylabels(ax)
+else:
+    # Just momentum chart
+    fig, ax = plt.subplots(figsize=(8, 4))
+    fig, ax = model.graph_momentum(ax=ax)
+    ax.set(ylabel="Performance", xlim=[0, model.match.shape[0]], ylim=[0, 1.1])
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.25))
 
-fig.align_ylabels(ax)
-
+fig.savefig('output/2021-champ.pdf', bbox_inches='tight')
 fig.show()
